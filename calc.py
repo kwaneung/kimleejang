93db04a925f559bef9e7aa68152cb8561efd5d92
sunbless <t11y048042@gmail.com>
2020-07-05 17:40:17 +0900

    success

--- calc.py
+++ calc.py
@@ -16,6 +16,10 @@ def Ddiv(x, y):
     hap = x / y
     print(str(x)+" / "+str(y)+" = "+str(round(hap, 2)))
 
+def Dmul(x,y):
+    hap = x * y
+    print(str(x) + "*" + str(y) + " = " + str(round(hap,2)))
+
 
 
 if __name__ == "__main__":
@@ -31,6 +35,7 @@ if __name__ == "__main__":
         Dsub(a, b)
     elif p == '*':
         print("곱셈 연산을 실행합니다.")
+        Dmul(a,b)
     elif p == '/':
         print("나눗셈 연산을 실행합니다.")
         Ddiv(a, b)
